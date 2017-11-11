21f1b5b6a5baa2a0925533a9846cacb428282048
Mark Fox-Smith <mark@fox-smith.co.uk>
2017-11-11 20:31:50 +0000

    Overrun and DHT Basics
    Switch the colour os the LEDs when the temprature goes out of range.
    Added basic features for the DHT temp/humidity sensor.

---
+++ DHT_Temp.ino
@@ -0,0 +1,9 @@
+void measure_DHT_values ()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+}
+
