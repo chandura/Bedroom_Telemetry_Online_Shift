9ed63da1ccdcf358c1014a1456462a99c3eb971d
Mark Fox-Smith <mark@fox-smith.co.uk>
2018-03-06 19:07:43 +0000

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -1,5 +1,3 @@
-import unittest as unit_test
-
 from clock_classes import stream_config
 from clock_classes import ranges
 from clock_functions import write_record
@@ -13,7 +11,7 @@ theranges = ranges(0.00, '00:00')
 config = stream_config(configgroup)
 streamer = Streamer(bucket_name=config.bucketname, bucket_key=config.bucketkey, access_key=config.accesskey)
 
-class TestBetween(unit_test.TestCase):
+class TestUM
 
     def test_config(self):
         config = stream_config(configgroup)
@@ -24,6 +22,7 @@ class TestBetween(unit_test.TestCase):
 
     def test_record_write_1(self):
         write = write_record(1, 100, streamer)
+        assert write_record(1, 100, streamer) = 'N'
         self.assertEqual(write, 'N')
 
     def test_record_write_100(self):
