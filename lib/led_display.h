10593622866641e137df3ce42edf08b006812ef4
Mark Fox-Smith <mark@fox-smith.co.uk>
2018-05-04 12:32:09 +0100

    Added the Morse example from the Arduino page
    This is the example of what the structure looks like.  This now needs changing out to include the actual functions and variables that we are going to use.

--- lib/led_display.h
+++ lib/led_display.h
@@ -1,1 +1,11 @@
 //This is the .h file for the library to display the digites on the LED display
+
+class LEDDisplay
+{
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+  private:
+    int _pin;
+};
