ea9fc0b666bb8ccfaa6a6a7657307f6e17eed656
Mark Fox-Smith <mark@fox-smith.co.uk>
2018-05-06 19:54:02 +0100

    Set time debug on

--- src/main.ino
+++ src/main.ino
@@ -20,7 +20,7 @@ dht DHT;
 #define PIN 12
 
 // Switches to switch on various special modes
-boolean verbose = 0;  //Switch this on (1) if you want to debugging prompts to be output
+boolean verbose = 1;  //Switch this on (1) if you want to debugging prompts to be output
 boolean setTheTime = false;  //Switch this on (1) if you want to reset the time on the RTC at restart.
 int debug = 0;        //A value of 0 swithces off all debugging
                       //A value of 1 swicthes on the debugging of the help message
